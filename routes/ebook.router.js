8f461119a9c95bd53774ad1fa85455f2a7de8f93
PrevailUzodinma <prevailuzodinma@gmail.com>
2024-05-16 13:43:50 +0100

    decided to implement the routes of saved books under ebook.router, so they can share a common url prefix '/ebook'

--- routes/ebook.router.js
+++ routes/ebook.router.js
@@ -1,10 +1,22 @@
 const router = require ("express").Router()
 const verifyToken = require("../middlewares/verifyToken.middleware.js")
 const eBookController = require("../controllers/ebook.controller.js");
+const SavedebookController = require("../controllers/savedebook.controller.js");
 
-
+// Route to search ebooks
 router.post('/', eBookController.searchEbooks);
-router.get('/:id',eBookController.searchEbooksById)
+
+// Route to search ebook by id
+router.get('/:id',eBookController.searchEbooksById);
+
+// Route to save an ebook
+router.post('/save', verifyToken, SavedebookController.saveEbook);
+
+// Route to get saved ebooks for a user
+router.get('/saved', verifyToken, SavedebookController.getSavedEbook);
+
+// Route to delete a saved ebook by ID
+router.delete('/saved/:id', verifyToken, SavedebookController.deleteSavedEbook);
 
 
 module.exports = router; 
