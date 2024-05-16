b9c5caef9d50bad94c51b9a6747c935310899ea5
PrevailUzodinma <prevailuzodinma@gmail.com>
2024-05-16 13:19:36 +0100

    added the models file for savedebook, to define the schema of how an ebook will be saved in the database

---
+++ models/savedebook.model.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const User = require("./user.model");
+const eBook = require("./ebook.model");
+
+const savedebookSchema = new mongoose.Schema({
+    ebookId:{
+        type: string,
+        required: true
+    },
+    userId:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    ebook:{
+        type: eBook.schema,
+        ref: 'User',
+        required: true,
+    }
+},     {
+    timestamps: true
+})
+
+const Savedebook = mongoose.model("Savedebook", savedebookSchema);
+module.exports = Savedebook
